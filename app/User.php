caa8e0da79486932cd1bca776abfcc5f4b398be5
wzjxh <wzjxh@af3c1572-f3fb-9e40-b51b-1579b8ceffda>
2016-12-21 02:45:06 +0000

    git-svn-id: https://39.108.144.9:8443/svn/yizhi@2 af3c1572-f3fb-9e40-b51b-1579b8ceffda

--- app/User.php
+++ app/User.php
@@ -9,31 +9,41 @@ use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use App\User;
 
 class User extends Model implements AuthenticatableContract,
                                     AuthorizableContract,
                                     CanResetPasswordContract
 {
     use Authenticatable, Authorizable, CanResetPassword;
-
-    /**
-     * The database table used by the model.
-     *
-     * @var string
-     */
     protected $table = 'users';
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = ['name', 'email', 'password'];
+    public static function saveUser($openid,$nickname,$img_url,$sex,$city){
+        $openid_user = User::where('wechat_openid',$openid)->first();
+        if($openid_user){
+            $openid_user->wechat_openid = $openid;
+            $openid_user->nickname = $nickname;
+            $openid_user->img_url = $img_url;
+            $openid_user->sex = $sex;
+            $openid_user->city = $city;
+            if($openid_user->save()){
+                return 200;
+            }else{
+                return 403;
+            }
+        }else{
+            $user = new User;
+            $user->wechat_openid = $openid;
+            $user->nickname = $nickname;
+            $user->img_url = $img_url;
+            $user->sex = $sex;
+            $user->city = $city;
+            if($user->save()){
+                return 200;
+            }else{
+                return 403;
+            }
+        }
 
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
-    protected $hidden = ['password', 'remember_token'];
+    }
 }
