fde9376e0dccd162cef3c075cd5a147fb766309a
wzjxh <wzjxh@af3c1572-f3fb-9e40-b51b-1579b8ceffda>
2017-01-11 12:28:51 +0000

    git-svn-id: https://39.108.144.9:8443/svn/yizhi@29 af3c1572-f3fb-9e40-b51b-1579b8ceffda

--- app/Http/Controllers/CommonController.php
+++ app/Http/Controllers/CommonController.php
@@ -47,7 +47,7 @@ class CommonController extends Controller
         if($request->msg == "get_brand_wcpay_request:ok"){
             //支付成功
             $order = Order::where('order_num',$request->order_num)->first();
-            $order->status = 'unfinished';
+            $order->status = 'payment';
             $order->save();
             $time = explode(' ' , $order->start_time);
             $overplus = Overplus::where('schedules_id',$order->schedules_id)->where('time',$time[0])->first();
@@ -67,12 +67,65 @@ class CommonController extends Controller
             return [
                 'code'=>200,
                 'detail'=>"支付成功",
+                'data' => [
+                    'number' => $order->number,
+                    'all_price' => $order->all_price,
+                    'order_num' => $order->order_num,
+                ]
             ];
+            // header("Location:http://yizhi.feibu.info/wechat/send_msg.php?number=".$order->number."&all_price=".$order->all_price."&order_num=".$order->order_num);
         }else if ($request->msg == "get_brand_wcpay_request:fail") {
             //支付失败
             return [
                 'code'=>403,
                 'detail'=>"支付失败",
+                'data' => [
+                    'order_num' => $order->order_num,
+                ]
+            ];
+        }
+    }
+
+    public function payOrder(Request $request){
+        $rules = [
+            'openid' => 'required',
+            'order_num' => 'required',
+        ];
+        if($this->validateParameter($request,$rules)!= 200){
+            return $this->validateParameter($request,$rules);
+        }
+        if($this->checkOpenid($request) != 200){
+            return $this->checkOpenid($request);
+        }
+
+        $getOrderDetail = Order::getOrderDetail($request->order_num);
+        //查询余票够不够
+        $schedules = Schedules::select(DB::raw('seat_number'))->where('id',$getOrderDetail->schedules_id)->first();
+        $time = explode(' ' , $getOrderDetail->start_time);
+        $overplus = Overplus::where('schedules_id',$getOrderDetail->schedules_id)->where('time',$time[0])->first();
+        if($overplus){
+            $overplus_num = $overplus->overplus;
+        }else{
+            $overplus_num = $schedules->seat_number;
+        }
+
+        if($getOrderDetail->number > $overplus_num){
+            return [
+                'code'=>403,
+                'detail'=>"余票不足",
+                'data' =>''
+            ];
+        }else{
+            $content = $getOrderDetail->start_details."--".$getOrderDetail->end_details;
+            return [
+                'code'=>200,
+                'detail'=>"请求成功",
+                'data' => [
+                    'content' => $content,
+                    'price' => $getOrderDetail->all_price,
+                    'order_num' =>  $request->order_num,
+                    'openid' =>  $request->openid,
+                ]
             ];
         }
     }
